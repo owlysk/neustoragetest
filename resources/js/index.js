08a76cbed0bd04ead878aa61f7fb799af67baefe
owlysk <martin.lacny@owly.sk>
2023-12-15 16:18:54 +0100

    added some tests

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,5 @@
 var i=0;
-
+/*
 async function writeToStorage(){
     i++;
     let date = Date();
@@ -17,6 +17,26 @@ async function writeToStorage(){
         console.log(error);
     });
 }
+*/
+
+function writeToStorage(){
+    (async() => {
+        i++;
+        let date = Date();
+        let d = date;
+        let data = `${i}: ${d}`;
+        console.log('data to write',data);
+        await Neutralino.storage.setData('setting', data )
+        .then(function(status){
+            Neutralino.os.showMessageBox("Setting", "Storage saved!")
+            console.log(status);
+        })
+        .catch(function(error){
+            Neutralino.os.showMessageBox("Setting", "Storage saving error!"+JSON.stringify(error))
+            console.log(error);
+        });
+    })();
+}
 
 async function readFromStorage(){
     console.log('reading from storage');
@@ -24,4 +44,22 @@ async function readFromStorage(){
     let data = await Neutralino.storage.getData('setting');
     Neutralino.os.showMessageBox("Setting", "Storage data: "+data)
     console.log(`Data: ${data}`);
+}
+
+async function writeFile(){
+    i++;
+    let date = Date();
+    let d = date;
+    let data = `${i}: ${d}`;
+    console.log('data to write',data);
+
+    await Neutralino.filesystem.writeFile('./myFile.txt', 'Sample content')
+    .then(function(status){
+        Neutralino.os.showMessageBox("File", "File saved!")
+        console.log(status);
+    })
+    .catch(function(error){
+        Neutralino.os.showMessageBox("File", "File saving error!"+JSON.stringify(error))
+        console.log(error);
+    });;
 }
